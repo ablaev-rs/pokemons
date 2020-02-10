78ba9e832e71974b6c3c7fade41a59d6a3ca7988
Rustem <ablaev.r.s@gmail.com>
2020-02-10 18:30:04 +0300

    Updates

---
+++ precache-manifest.84d9ce7955dccb39cc2a67b70afe1118.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e274761cc5cea297d52091fc7093d8c1",
+    "url": "/ablaev-rs/pokemons.git/index.html"
+  },
+  {
+    "revision": "707b1085d161a3029e26",
+    "url": "/ablaev-rs/pokemons.git/static/css/main.b63c19c9.chunk.css"
+  },
+  {
+    "revision": "86f9280c5300433a8b3b",
+    "url": "/ablaev-rs/pokemons.git/static/js/2.73302bfc.chunk.js"
+  },
+  {
+    "revision": "6b8a8ed29fc352ee81d4452089aa1005",
+    "url": "/ablaev-rs/pokemons.git/static/js/2.73302bfc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "707b1085d161a3029e26",
+    "url": "/ablaev-rs/pokemons.git/static/js/main.0f1213aa.chunk.js"
+  },
+  {
+    "revision": "77916005422182fdfa8d",
+    "url": "/ablaev-rs/pokemons.git/static/js/runtime-main.6fa12084.js"
+  },
+  {
+    "revision": "2b1594bbbe85dde86c22009dc01cc7a0",
+    "url": "/ablaev-rs/pokemons.git/static/media/defaultAvatar.2b1594bb.png"
+  }
+]);
