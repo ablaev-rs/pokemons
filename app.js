2179a9ea6e2fe35046e512ecc42de5925d3df7c1
Rustem Ablaev <ablaev.r.s@gmail.com>
2020-03-24 21:52:10 +0300

    fix

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const path = require('path');
 const app = express();
 
 const config = require('config');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env["PORT"] || 5000;
 
 app.use(express.json({ extended: true }));
 
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/favorite', require('./routes/favorite.routes'));
 
-if(process.env.NODE_ENV === 'production') {
+if(process.env["NODE_ENV"] === 'production') {
     app.use(express.static(path.join('client/build')));
     add.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
@@ -20,7 +20,7 @@ if(process.env.NODE_ENV === 'production') {
 
 async function start() {
     try {
-        await mongoose.connect(process.env.MONGODB_URI || config.get('mongoUri'), {
+        await mongoose.connect(process.env["MONGODB_URI"] || config.get('mongoUri'), {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
