3bbdb6288a5d01dc84187b6dcf2aa946e1b64861
Rustem Ablaev <ablaev.r.s@gmail.com>
2020-03-24 20:17:15 +0300

    heroku settings

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const config = require('config');
@@ -10,9 +11,16 @@ app.use(express.json({ extended: true }));
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/favorite', require('./routes/favorite.routes'));
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join('client/build'));
+    add.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 async function start() {
     try {
-        await mongoose.connect(config.get('mongoUri'), {
+        await mongoose.connect(process.env.MONGODB_URI || config.get('mongoUri'), {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
