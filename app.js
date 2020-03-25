9050ac051d239825abf92f4176651e7bad9f1fd0
Rustem Ablaev <ablaev.r.s@gmail.com>
2020-03-25 13:14:05 +0300

    fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const path = require('path');
 const app = express();
+const path = require('path');
 
 const config = require('config');
 const PORT = process.env["PORT"] || 5000;
@@ -13,6 +13,9 @@ app.use('/api/favorite', require('./routes/favorite.routes'));
 
 if(process.env["NODE_ENV"] === 'production') {
     app.use(express.static('client/build'));
+    app.get('*', (req, res) =>{
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
 }
 
 async function start() {
