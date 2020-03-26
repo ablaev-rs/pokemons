be261ae73de514cae54636301d1581709b4a160e
Rustem Ablaev <ablaev.r.s@gmail.com>
2020-03-26 15:39:08 +0300

    fix

--- app.js
+++ app.js
@@ -29,13 +29,6 @@ if(process.env["NODE_ENV"] !== 'production') {
         response.sendFile(path.resolve(__dirname, './dist/index.html'));
     });
 }
-/*
-if(process.env["NODE_ENV"] !== 'production') {
-    app.use('/', express.static(path.join(__dirname, './dist/')));
-    app.get('*', function(request, response) {
-        response.sendFile(path.resolve(__dirname, '/client/build/index.html'));
-    });
-}*/
 
 async function start() {
     try {
