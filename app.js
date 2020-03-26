f029dc0f8cfefcb3ef829ada4c6e7552823eb6cc
Rustem Ablaev <ablaev.r.s@gmail.com>
2020-03-26 17:52:35 +0300

    fix

--- app.js
+++ app.js
@@ -14,7 +14,12 @@ app.options('*', cors());
 
 app.use(express.json({ extended: true }));
 
+app.use(function(req, res, next) {
+    res.setHeader("Content-Security-Policy", "script-src 'self' https://fonts.googleapis.com https://pokeapi.co https://raw.githubusercontent.com");
+    return next();
+});
 
+/*
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Content-Security-Policy", "default-src 'SELF'");
@@ -42,7 +47,7 @@ app.use(expressCspHeader({
         'block-all-mixed-content': true
     }
 }));
-
+*/
 
 
 
