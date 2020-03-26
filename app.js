44ffea4385dd6a41a0e952db478f04d1ff19b02c
Rustem Ablaev <ablaev.r.s@gmail.com>
2020-03-26 17:48:37 +0300

    fix

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ app.options('*', cors());
 
 app.use(express.json({ extended: true }));
 
-/*
+
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Content-Security-Policy", "default-src SELF, https://fonts.googleapis.com, https://pokeapi.co, https://raw.githubusercontent.com");
+    res.header("Content-Security-Policy", "default-src 'SELF'");
     res.header("X-Content-Security-Policy", "default-src *");
     res.header("X-WebKit-CSP", "default-src *");
     res.header(
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
         return res.status(200).json({});
     }
     next();
-});*/
+});
 
 app.use(expressCspHeader({
     directives: {
